91b7f57b2c7361734fc2758044a4eef57138a817
Taeseung Lee <taeseunglee159@gmail.com>
2018-05-04 08:58:47 +0000

    Fix some bugs for running this program

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
+const port = 8000;
 
 // set routers
 // base path: "./route"
 // Result Path : base path + routeName + ".js"
 // example : "./route" + "/home" + ".js" => ./route/home.js
-const routes = ["/browse"];
-for (var routeName in routes) {
-    var router = require("./route" + routeName + ".js")(app);
+const routes = ["/template"];
+for (var idx in routes) {
+    var router = require('./route' + routes[idx] + '.js')(app);
+
     // mount the router on the app
-    app.use(routeName, router);
+    app.use(routes[idx], router);
 }
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.listen(port, () => console.log('Example app listening on port 8000!'))
