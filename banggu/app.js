68cd109558b52bc5e193252ef56eb45675a0b865
Taeseung Lee <taeseunglee159@gmail.com>
2018-06-08 02:42:08 +0900

    Add Acceess control-allow headers

--- banggu/app.js
+++ banggu/app.js
@@ -4,6 +4,25 @@ const path = require("path");
 
 const port = 8000;
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', "*");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(express.static(__dirname + '/view'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/view/home.html')));
